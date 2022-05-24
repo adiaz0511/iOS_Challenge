57e23f8b2ab25ba0dc7a65300b1464a58d2d2902
Arturo <adiaz0511@hotmail.com>
2022-05-24 11:50:04 -0500

    Initial Commit

---
+++ iOS_Challenge/iOS_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ChallengeApp.swift
+//  iOS_Challenge
+//
+//  Created by Arturo Diaz on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
